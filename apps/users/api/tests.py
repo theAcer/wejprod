816bff164b1d6cb9fc6851ef3d91a116e630f383
theAcer <samsonnjogu@gmail.com>
2023-10-28 14:53:53 +0000

    users api

--- apps/users/api/tests.py
+++ apps/users/api/tests.py
@@ -1,17 +1,65 @@
 from django.test import TestCase
+from users.models import CustomUser
 from rest_framework import status
 from rest_framework.test import APIClient
 from django.contrib.auth import get_user_model
 from django.urls import reverse  # Import the reverse function
 
-class UserRegistrationAPITest(TestCase):
+
+class login:
+    def __init__(self, testcase, user, password):
+        self.testcase = testcase
+        success = testcase.client.login(username=user, password=password)
+        self.testcase.assertTrue(
+            success, f"login with username={user!r}, password={password!r} failed"
+        )
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self, *args):
+        self.testcase.client.logout()
+
+
+class BaseTestCase(TestCase):
     def setUp(self):
-        self.client = APIClient()
-        self.user_data = {
-            'username': 'testuser',
-            'email': 'testuser@example.com',
-            'password': 'password123',
-        }
+        """
+        Setup some initial users
+
+        """
+        self.user_pw = "test"
+        self.user_bob = self.create_user("bob", "bob@bob.com", self.user_pw)
+        self.user_steve = self.create_user("steve", "steve@steve.com", self.user_pw)
+        self.user_susan = self.create_user("susan", "susan@susan.com", self.user_pw)
+        self.user_amy = self.create_user("amy", "amy@amy.amy.com", self.user_pw)
+        cache.clear()
+
+    def tearDown(self):
+        cache.clear()
+        self.client.logout()
+
+    def login(self, user, password):
+        return login(self, user, password)
+
+    def create_user(self, username, email_address, password):
+        user = CustomUser.objects.create_user(username, email_address, password)
+        return user
+
+    def assertResponse200(self, response):
+        self.assertEqual(response.status_code, 200)
+
+    def assertResponse302(self, response):
+        self.assertEqual(response.status_code, 302)
+
+    def assertResponse403(self, response):
+        self.assertEqual(response.status_code, 403)
+
+    def assertResponse404(self, response):
+        self.assertEqual(response.status_code, 404)
+
+
+class UserRegistrationAPITest(BaseTestCase):
+
 
     def test_user_registration(self):
         url = reverse('users:user-registration')  # Use the reverse function with the URL name
--- apps/users/tests.py
+++ apps/users/tests.py
@@ -6,79 +6,96 @@ from django.core.files.uploadedfile import SimpleUploadedFile
 from users.models import CustomUser, Player
 from .forms import CustomUserCreationForm
 from .views import SignupPageView
+from django.core.cache import cache
 
-
-class CustomUserTests(TestCase):
-    def test_create_user(self):
-        User = get_user_model()
-        user = User.objects.create_user(
-        username='will',
-        email='will@email.com',
-        password='testpass123'
+class login:
+    def __init__(self, testcase, user, password):
+        self.testcase = testcase
+        success = testcase.client.login(username=user, password=password)
+        self.testcase.assertTrue(
+            success, f"login with username={user!r}, password={password!r} failed"
         )
 
-        self.assertEqual(user.username, 'will')
-        self.assertEqual(user.email, 'will@email.com')
-        self.assertTrue(user.is_active)
-        self.assertFalse(user.is_staff)
-        self.assertFalse(user.is_superuser)
+    def __enter__(self):
+        passclear
 
-    def test_create_superuser(self):
-        User = get_user_model()
-        admin_user = User.objects.create_superuser(
-        username='superadmin',
-        email='superadmin@email.com',
-        password='testpass123'
-        )
-        self.assertEqual(admin_user.username, 'superadmin')
-        self.assertEqual(admin_user.email, 'superadmin@email.com')
-        self.assertTrue(admin_user.is_active)
-        self.assertTrue(admin_user.is_staff)
-        self.assertTrue(admin_user.is_superuser)
+    def __exit__(self, *args):
+        self.testcase.client.logout()
 
-class SignupPageTests(TestCase):
-    username = "newuser"
-    email = "newuser@email.com"
 
+class BaseTestCase(TestCase):
     def setUp(self):
-        url = reverse('account_signup')
-        self.response = self.client.get(url)
+        """
+        Setup some initial users
 
+        """
+        self.user_pw = "test"
+        self.user_bob = self.create_user("bob", "bob@bob.com", self.user_pw)
+        self.user_steve = self.create_user("steve", "steve@steve.com", self.user_pw)
+        self.user_susan = self.create_user("susan", "susan@susan.com", self.user_pw)
+        self.user_amy = self.create_user("amy", "amy@amy.amy.com", self.user_pw)
+        cache.clear()
+
+    def tearDown(self):
+        cache.clear()
+        self.client.logout()
+
+    def login(self, user, password):
+        return login(self, user, password)
+
+    def create_user(self, username, email_address, password):
+        user = CustomUser.objects.create_user(username, email_address, password)
+        return user
+
+    def assertResponse200(self, response):
+        self.assertEqual(response.status_code, 200)
+
+    def assertResponse302(self, response):
+        self.assertEqual(response.status_code, 302)
+
+    def assertResponse403(self, response):
+        self.assertEqual(response.status_code, 403)
+
+    def assertResponse404(self, response):
+        self.assertEqual(response.status_code, 404)
+
+class SignupPageTests(BaseTestCase):
     def test_signup_template(self):
-        self.assertEqual(self.response.status_code, 200)
-        self.assertTemplateUsed(self.response, 'account/signup.html')
-        self.assertContains(self.response, 'Sign Up')
-        self.assertNotContains(
-        self.response, 'Hi there! I should not be on the page.')
+        url = reverse('account_signup')
+        response = self.client.get(url)
 
-
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'account/signup.html')
+        self.assertContains(response, 'Sign Up')
+        self.assertNotContains(response, 'Hi there! I should not be on the page.')
 
     def test_signup_form(self):
-        new_user = get_user_model().objects.create_user(self.username, self.email)
+        new_user = get_user_model().objects.create_user('testusername', 'testemail@example.com')  # Create a new user
         self.assertEqual(get_user_model().objects.all().count(), 1)
-        self.assertEqual(get_user_model().objects.all()[0].username, self.username)
-        self.assertEqual(get_user_model().objects.all()[0].email, self.email)
+        self.assertEqual(get_user_model().objects.all()[0].username, 'testusername')
+        self.assertEqual(get_user_model().objects.all()[0].email, 'testemail@example.com')
 
 
-class PlayerModelTest(TestCase):
-    
+class PlayerModelTest(BaseTestCase):   
     def setUp(self):
-        # Create a user if it doesn't exist, or get an existing user
-        self.user, created = CustomUser.objects.get_or_create(username='testuser', email='test@example.com', password='testpassword')
+        super().setUp()  # Call the setUp method of the base class
+        # Reference the users created in the base class
+        self.user = self.user_bob  # Replace 'user_bob' with the actual user you want to use
         self.player = Player.objects.create(user=self.user, name='Test Player', handicap=10)
 
     def tearDown(self):
-        # Clean up after each test by deleting the player and user
+        # Clean up after each test by deleting the player
         self.player.delete()
-        self.user.delete()
+        super().tearDown()  # Call the tearDown method of the base class
 
     def test_player_str(self):
         self.assertEqual(str(self.player), 'Test Player')
 
     def test_player_absolute_url(self):
-        url = self.player.get_absolute_url()
-        expected_url = reverse('users:profile')
-        self.assertEqual(url, expected_url)
+        with self.login(self.user.username, self.user_pw):  # Use the user's username and password from the base class
+            url = self.player.get_absolute_url()
+            expected_url = reverse('users:profile')
+            self.assertEqual(url, expected_url)
 
     def test_player_creation(self):
         self.assertTrue(Player.objects.filter(user=self.user, name='Test Player', handicap=10).exists())
