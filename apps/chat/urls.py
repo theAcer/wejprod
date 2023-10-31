a6a3a4783d3473b1996176fd27f37c425730ddfb
theAcer <samsonnjogu@gmail.com>
2023-10-31 17:50:11 +0000

    2much sauce

--- apps/chat/urls.py
+++ apps/chat/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from app.views import chat_box
 
+app_name = 'chat'
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("chat/<str:chat_box_name>/", chat_box, name="chat"),
